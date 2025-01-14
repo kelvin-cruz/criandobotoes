315051af47aea35801ea742841cb03c95f82f42f
kelvincruz <kelvincruz.dev@outlook.com>
2025-01-13 21:32:41 -0300

    parte de css global e não global

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import "./components/global.css"
+import"./components/index.css"
 import {Button} from "./components/Button"
+
+
 
 export function App (){
     return(
-        <div>
-            <Button name="kelvin"/>
-            <Button/>
-            <Button/>
+        <div className="container">
+            <Button name="Adicionar"/>
+            <span>0</span>
+            <Button name="Remover"/>
         </div>
         
     )
