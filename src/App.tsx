a31935f1072444a5ee970ccaff2513d4a96b5a4b
kelvincruz <kelvincruz.dev@outlook.com>
2025-01-15 18:55:23 -0300

    proximopassohook

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import "./components/global.css"
-import"./components/index.css"
-import {Button} from "./components/Button"
-
-
-
 export function App (){
     return(
-        <div className="container">
-            <Button name="Adicionar"/>
-            <span>0</span>
-            <Button name="Remover"/>
-        </div>
-
+        <h1>oie kelvin!</h1>
     )
 }
